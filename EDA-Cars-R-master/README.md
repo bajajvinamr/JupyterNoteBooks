1a3aac08d14cb9ced90bb33507e9ba273df6b32b
bajajvinamr <lit2019023@iiitl.ac.in>
2021-04-24 05:08:07 +0530

    Added Notebooks

---
+++ EDA-Cars-R-master/README.md
@@ -0,0 +1,2 @@
+# EDA-Cars-R
+Attempt at Expolratory Data Analysis of the Cars Data Set in R
---
+++ EDA-Cars-R-master/R_Exercise_2_LIT2019023.R
@@ -0,0 +1,7 @@
+jpeg("exercise2plot.jpg")
+plot(cars, xlab = "Speed (mph)", ylab = "Stopping distance (ft)",
+     las = 1)
+lines(lowess(cars$speed, cars$dist, f = 2/3, iter = 3), col = "red")
+title(main = "Cars Dataset Exercise")
+dev.off() 
+
